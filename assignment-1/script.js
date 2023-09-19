7f00cd24818d0532bc6c6b39fc47751dd855c08e
Acerchicken <phamxuanduong1310@gmail.com>
2023-09-20 00:12:38 +0700

    Commit lần 3
    Fix lại chức năng Delete khi thêm bớt sách

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,5 +1,8 @@
 // Your JS code goes here
 
+// Declare bookTableBody variable
+let bookTableBody = document.getElementById('bookTableBody');
+
 //Nhập input để Search
 document.getElementById('searchInput').addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
@@ -34,80 +37,46 @@ function togglePopup(popupID){
     popupID.classList.toggle("active");
 }
 
-//Thêm sách
+// Thêm sách và đóng popup
 function addBookAndClosePopup(name_cpy, author_cpy, topic_cpy) {
     // Lấy giá trị từ các trường input
-
     let newBook = {
         name: name_cpy,
         author: author_cpy,
         topic: topic_cpy,
     };
     books.push(newBook);
-    //Reset input
+    // Reset input
     document.getElementById('nameInput').value = "";
     document.getElementById('authorInput').value = "";
     document.getElementById('topicInput').value = "";
     // Đóng popup
     togglePopup(DialogConfirm);
     togglePopup(popupMain);
+
+    // Reset bookToDeleteIndex
+    bookToDeleteIndex = undefined;
+
     renderTable();
 }
 
-// Xác nhận xóa cuốn sách
-function deleteConfirmed() {
-    if (typeof bookToDeleteIndex !== 'undefined'){
-        books.splice(bookToDeleteIndex, 1);
-        renderTable(); // Cập nhật lại bảng
-        togglePopup(confirmDeletePopup); // Đóng dialog xác nhận
-        bookToDeleteIndex = undefined; // Đặt lại giá trị của biến
-    }
-}
 
-//Khai báo Mảng object Books
+// Khai báo Mảng object Books
 let books = [
     { name: "Refactoring", author: "Martin Fowler", topic: "Programming" },
     { name: "Designing Data-Intensive Applications", author: "Martin Kleppmann", topic: "Database" },
     { name: "The Phoenix Project", author: "Gene Kim", topic: "DevOps" }
 ];
-//Chỉ số của sách cần xoá
+
+// Chỉ số của sách cần xoá
 let bookToDeleteIndex;
-//Clone danh sách để back-up
+
+// Clone danh sách để back-up
 let initialBooks = [...books];
 
-let bookTableBody = document.getElementById('bookTableBody');
-//Duyệt từng phần tử book
-for (let [index, book] of books.entries()) {
-    let row = bookTableBody.insertRow();
-        
-    let nameCell = row.insertCell();
-    nameCell.textContent = book.name;
-            
-    let authorCell = row.insertCell();
-    authorCell.textContent = book.author;
-            
-    let topicCell = row.insertCell();
-     topicCell.textContent = book.topic;
-            
-    //Cột Action
-    let actionCell = row.insertCell();
-    let deleteButton = document.createElement("span");
-    deleteButton.textContent = "Delete";
-    deleteButton.classList.add("delete-text");
-    // Thực hiện hành động xoá object
-    deleteButton.addEventListener("click", function() {
-    //Xoá 1 phần tử bắt đầu từ vị trí thứ index+1
-    bookToDeleteIndex = index;
-    togglePopup(confirmDeletePopup);
-    });
-    actionCell.appendChild(deleteButton);
-};
-
-
-//Cập nhật lại bảng
+// Cập nhật lại bảng
 function renderTable(){
     let bookTableBody = document.getElementById('bookTableBody');
-    console.log('Rendered books:', books); // Kiểm tra dữ liệu trước khi cập nhật
     let tableHTML = books.map((book, index) => `
         <tr>
             <td>${book.name}</td>
@@ -122,9 +91,45 @@ function renderTable(){
     bookTableBody.innerHTML = tableHTML;
 }
 
+function deleteConfirmed() {
+    if (typeof bookToDeleteIndex !== 'undefined') {
+        books.splice(bookToDeleteIndex, 1);
+        renderTable(); // Cập nhật lại bảng
+        togglePopup(confirmDeletePopup); // Đóng dialog xác nhận
+        bookToDeleteIndex = undefined; // Đặt lại giá trị của biến
+    }
+}
+
+// Define deleteBook function
+function deleteBook(index) {
+    bookToDeleteIndex = index;
+    togglePopup(confirmDeletePopup);
+}
 
 
-
-
-
-
+// Duyệt từng phần tử book
+for (let [index, book] of books.entries()) {
+    let row = bookTableBody.insertRow();
+        
+    let nameCell = row.insertCell();
+    nameCell.textContent = book.name;
+            
+    let authorCell = row.insertCell();
+    authorCell.textContent = book.author;
+            
+    let topicCell = row.insertCell();
+    topicCell.textContent = book.topic;
+            
+    // Cột Action
+    let actionCell = row.insertCell();
+    let deleteButton = document.createElement("span");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-text");
+    // Thực hiện hành động xoá object
+    deleteButton.addEventListener("click", function() {
+        // Lấy index của cuốn sách trong danh sách hiện tại
+        let index = this.parentElement.parentElement.rowIndex - 1;
+        deleteBook(index);
+    });
+    actionCell.appendChild(deleteButton);
+}
